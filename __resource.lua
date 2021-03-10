d3fa5ff55376a31b1796b2d06d3e74161b8c0ad1
Ocuro <74819581+WolfScripting@users.noreply.github.com>
2021-03-10 09:14:31 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,5 @@
+description 'Disables Close Combat (Removes the one hit kill) by OcuroTheShepherd'
+
+client_script "client.lua"
+
+
---
+++ client.lua
@@ -0,0 +1,16 @@
+--Created by OcuroTheShepherd
+--Contact me on my Discord-Server: https://discord.gg/55WzYKyzGh
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+		local ped = GetPlayerPed( -1 )
+		local weapon = GetSelectedPedWeapon(ped)
+		if IsPedArmed(ped, 6) then
+        	DisableControlAction(1, 140, true)
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
+        end
+    end
+end)
+
